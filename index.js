2d29a5df51379667a4fe737dc7a51441c72f25be
KRBdFirst <reshybella10@gmail.com>
2022-06-04 21:40:45 +0100

    update menu icon

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let menuItems = document.querySelector('.menu-items')
 let mobToggle = document.querySelector('.mob-toggle')
+let toggleIcon = document.querySelector('#toggle-image')
 
 mobToggle.addEventListener('click', () => {
     const visibility = menuItems.getAttribute('data-visible')
     if (visibility === "false") {
         menuItems.setAttribute('data-visible', true)
+        toggleIcon.src = "images/x.png"
     }
     else {
         menuItems.setAttribute('data-visible', false)
+        toggleIcon.src = "images/menu.png"
     }
 })
